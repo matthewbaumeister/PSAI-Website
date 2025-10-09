1d4c7f72af634421c73e88377ac7c7a5198f1b39
Matthew Baumeister <matt@prop-shop.ai>
2025-10-09 12:40:40 -0400

    Replace Vercel favicon with PSAI logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,6 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: '/favicon.svg', type: 'image/svg+xml' },
-      { url: '/favicon.ico', sizes: 'any' }
     ],
     apple: '/favicon.svg',
   },
